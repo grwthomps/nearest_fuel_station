08eaf5c0ecd12bd9c6630b6197821ca84ac0c7ef
Graham Thompson <grwthomps@gmail.com>
2019-12-20 10:13:57 -0700

    Temporarily get nrel api call working in search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,14 @@
 class SearchController < ApplicationController
   def index
+  station_response = Faraday.get('https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json') do |req|
+    req.params['api_key'] = ENV['nrel_key']
+    req.params['fuel_type'] = ENV['ELEC']
+    req.params['location'] = '1331+17th+St+Denver+CO'
+    req.params['limit'] = 1
+  end
 
+  station_data = JSON.parse(response.body, symbolize_names: true)
+  nearest_station = data[:fuel_stations][0]
+  binding.pry
   end
 end
