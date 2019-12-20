8b92d9624417e975255e086848eb2b509aa3cdcc
Graham Thompson <grwthomps@gmail.com>
2019-12-20 10:56:42 -0700

    Remove commented out user story from test

--- spec/features/user_sees_closest_electric_fuel_station_spec.rb
+++ spec/features/user_sees_closest_electric_fuel_station_spec.rb
@@ -12,10 +12,3 @@ describe "As a visitor on the search page" do
     expect(page).to have_content('MO: Not Specified; TU: Not Specified; WE: Not Specified; TH: Not Specified; FR: Not Specified; SA: Not Specified; SU: Not Specified')
   end
 end
-
-# Then I should see the closest electric fuel station to me.
-# For that station I should see
-# - Name
-# - Address
-# - Fuel Type
-# - Access Times
